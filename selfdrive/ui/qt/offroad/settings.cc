1843593ed746fdc4fc4ae43a7a63d77005b38550
Vehicle Researcher <>
2021-04-06 15:54:43 +0900

    openpilot v0.8.3

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -297,7 +297,6 @@ QWidget * network_panel(QWidget * parent) {
   layout->addWidget(new ButtonControl("믹스플로러", "실행", "파일 및 기타 유지관리를 위해 믹스플로러를 실행합니다. 믹스플로러는 다양한 기능(앱실행, 코드수정 등)을 이용할 수 있습니다.",
                                       [=]() {
                                         std::system(run_mixplorer);
-                                        closeSettings();
                                       }));
 
   layout->addWidget(horizontal_line());
