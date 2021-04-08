a4dac5e222b56c90d0d3a20c2308fdebc316c304
Vehicle Researcher <>
2021-04-08 14:42:19 +0900

    dd

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/bash
 
-if [ ! -f "/data/no_ota_updates" ]; then
-    /usr/bin/touch /data/no_ota_updates
-fi
+# if [ ! -f "/data/no_ota_updates" ]; then
+#     /usr/bin/touch /data/no_ota_updates
+# fi
 
-ALIAS_CHECK=$(/usr/bin/grep gitpull /system/comma/home/.bash_profile)
+# ALIAS_CHECK=$(/usr/bin/grep gitpull /system/comma/home/.bash_profile)
 
-if [ "$ALIAS_CHECK" == "" ]; then
-    sleep 3
-    mount -o remount,rw /system
-    echo "alias gi='/data/openpilot/gitpull.sh'" >> /system/comma/home/.bash_profile
-    mount -o remount,r /system
-fi
+# if [ "$ALIAS_CHECK" == "" ]; then
+#     sleep 3
+#     mount -o remount,rw /system
+#     echo "alias gi='/data/openpilot/gitpull.sh'" >> /system/comma/home/.bash_profile
+#     mount -o remount,r /system
+# fi
 
-if [ ! -f "/data/KRSet" ]; then
-    setprop persist.sys.locale ko-KR
-    setprop persist.sys.local ko-KR
-    setprop persist.sys.timezone Asia/Seoul
-    /usr/bin/touch /data/KRSet
-fi
+# if [ ! -f "/data/KRSet" ]; then
+#     setprop persist.sys.locale ko-KR
+#     setprop persist.sys.local ko-KR
+#     setprop persist.sys.timezone Asia/Seoul
+#     /usr/bin/touch /data/KRSet
+# fi
 
 if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     sleep 3
