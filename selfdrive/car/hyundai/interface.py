80769fa329718e84f4826e6f9c438761ad60e3a9
openpilotusers <>
2021-04-09 18:48:23 +0900

    dd

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -303,7 +303,10 @@ class CarInterface(CarInterfaceBase):
     self.CS.out = ret.as_reader()
     return self.CS.out
 
-  def apply(self, c, sm, CP):
-    can_sends = self.CC.update(c, self.CS, self.frame, sm, CP )
+  def apply(self, c, sm):
+    can_sends = self.CC.update(c.enabled, self.CS, self.frame, c, c.actuators,
+                               c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
+                               c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
+                               c.hudControl.setSpeed, c.hudControl.leadVisible, sm)
     self.frame += 1
     return can_sends
